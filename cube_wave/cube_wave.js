064eff68c142a7eb10c405a181f6878a2b133e63
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-03 10:31:59 +0530

    Orthographic projection added cube wave

--- cube_wave/cube_wave.js
+++ cube_wave/cube_wave.js
@@ -1,44 +1,32 @@
-// let gifLength = 600;
-// let canvas;
-
 let angle = 0;
-let w = 24;
 
 
-function setup(){
+function setup() {
     createCanvas(800, 800, WEBGL);
-    // canvas = p5Canvas.canvas;
-    // capturer.start();
 }
 
-function draw(){
+function draw() {
     background(255);
-    rotateX(- Math.PI / 4);
-    rotateY( - Math.PI / 4);
-    
-    fill('#AED6F1');
+    translate(0, 50, 0);
+    ortho(-400, 400, -400, 400, 0, 1000);
+    rotateX(- Math.PI / 5);
+    rotateY(- Math.PI / 4);
 
-    for(let z = -180 ; z <= 180; z += 30){
-        for(let x = -180; x <= 180 ; x += 30){
+    fill('#F9E79F ');
+
+    for (let z = -160; z <= 160; z += 20) {
+        for (let x = -160; x <= 160; x += 20) {
             push();
             translate(x, 0, z);
             let d = dist(x, z, 0, 0);
-            let offset = map(d, -150, 150, -2, 2);
+            let offset = map(d, -160, 160, -6, 6);
             let a = angle + offset;
             let h = map(sin(a), -1, 1, 0, 200);
-            box(20,h,20);
+            box(20, h, 20);
             pop();
         }
     }
 
     angle -= 0.04;
-
-    // if(frameCount < gifLength){
-    //     capturer.capture(canvas);
-    // }
-    // else if(frameCount === gifLength){
-    //     capturer.stop();
-    //     capturer.save();
-    // }
 
 }
