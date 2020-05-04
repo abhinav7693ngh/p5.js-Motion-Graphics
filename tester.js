97a023af8c0dd0b742bda83d813eaaa956539ff4
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-05 04:43:44 +0530

    Moving from one point to another

--- tester.js
+++ tester.js
@@ -26,43 +26,74 @@
 
 
 
-let angle = 0;
+// let angle = 0;
 
-function setup() {
-    createCanvas(900, 900, WEBGL);
-}
+// function setup() {
+//     createCanvas(900, 900, WEBGL);
+// }
 
-function draw() {
-    let v = createVector(0,-1,0);
-    // v.normalize();
-    // directionalLight(255, 255, 0, v);
-    background(0);
+// function draw() {
+//     let v = createVector(0,-1,0);
+//     // v.normalize();
+//     // directionalLight(255, 255, 0, v);
+//     background(0);
 
-    //camera(0,0,(height / 2) / tan(PI/6),0,0,0,0,1,0);
+//     //camera(0,0,(height / 2) / tan(PI/6),0,0,0,0,1,0);
 
     
 
-    ambientLight(255);
+//     ambientLight(255);
 
 
-    noStroke();
-    //fill(255,0,150);
+//     noStroke();
+//     //fill(255,0,150);
 
-    // stroke(0);
+//     // stroke(0);
 
-    normalMaterial();
-    // ambientMaterial(255);
-    //specularMaterial(255);
-    // translate(mouseX, mouseY);
-    rectMode(CENTER);
-    rotateX(angle);
-    rotateY(angle * 0.4);
-    rotateZ(angle * 1.2);
-    //rect(0,0,150,150);
+//     normalMaterial();
+//     // ambientMaterial(255);
+//     //specularMaterial(255);
+//     // translate(mouseX, mouseY);
+//     rectMode(CENTER);
+//     rotateX(angle);
+//     rotateY(angle * 0.4);
+//     rotateZ(angle * 1.2);
+//     //rect(0,0,150,150);
 
-    box(100,100,100);
-    //sphere(100);
-    // plane(200,200);
+//     box(100,100,100);
+//     //sphere(100);
+//     // plane(200,200);
 
-    angle += 0.08;
+//     angle += 0.08;
+// }
+
+let updatedX;
+let updatedY;
+let r, theta, myLine, rUse = 0, x, y;
+
+
+function setup(){
+    createCanvas(400,400);
+    angleMode(DEGREES);
+    frameRate(60);
+    myLine = createVector(50, 50, 300, 300);
+    theta = atan2(myLine.y, myLine.x);
+    r = dist(50, 50, 300, 300);
+    x = myLine.x;
+    y = myLine.y;
+}
+
+function draw(){
+    fill(255);
+    background(172);
+    if(rUse > r){
+        ellipse(300, 300, 20);
+    }
+    else{
+        x = myLine.x + rUse * cos(theta);
+        y = myLine.y + rUse * sin(theta);
+        rUse += 1;
+        ellipse(x, y, 20);
+    }
+    
 }
